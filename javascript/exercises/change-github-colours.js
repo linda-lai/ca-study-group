089cc77388ff2b3be5ea12a8179e8b25fa211424
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2018-11-14 13:51:39 +1100

    js DOM exercise

---
+++ javascript/exercises/change-github-colours.js
@@ -0,0 +1,27 @@
+// Inspect your github profile and paste this code to see the colours change. 
+
+const array = document.getElementsByClassName("day")
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+  
+
+let colours = ["red",
+"blue",
+"green",
+"yellow"
+]
+
+
+function changeColour () {
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        element.style.fill = colours[getRandomInt(4)]
+    }
+}
+
+
+
+
+
